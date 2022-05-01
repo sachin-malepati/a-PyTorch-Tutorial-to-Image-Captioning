ab0df6784c22d59aa6f70caeb833b7fbe629fe3c
sachin-malepati <100379121+sachin-malepati@users.noreply.github.com>
2022-05-01 17:20:58 -0400

    update

--- create_input_files.py
+++ create_input_files.py
@@ -3,7 +3,7 @@ from utils import create_input_files
 if __name__ == '__main__':
     # Create input files (along with word map)
     create_input_files(dataset='coco',
-                       karpathy_json_path='../caption data/dataset_coco.json',
+                       karpathy_json_path='../coco/dataset.json',
                        image_folder='/media/ssd/caption data/',
                        captions_per_image=5,
                        min_word_freq=5,

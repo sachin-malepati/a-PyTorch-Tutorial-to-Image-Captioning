5710c657dd3bd56289091456e535022a5e843e06
sachin-malepati <100379121+sachin-malepati@users.noreply.github.com>
2022-05-02 20:28:39 -0400

    Update create_input_files.py

--- create_input_files.py
+++ create_input_files.py
@@ -3,9 +3,9 @@ from utils import create_input_files
 if __name__ == '__main__':
     # Create input files (along with word map)
     create_input_files(dataset='coco',
-                       karpathy_json_path='../coco/dataset.json',
-                       image_folder='./media/ssd/caption_data/',
+                       karpathy_json_path='/home/amm9801/coco/dataset.json',
+                       image_folder='/coco/',
                        captions_per_image=5,
                        min_word_freq=5,
-                       output_folder='./media/ssd/caption_data/',
+                       output_folder='/home/amm9801/temp_op/',
                        max_len=50)
